021f5be06e9814808cfba3eebf62d510e739d611
Alex <AlexanderDaniel.Berrang@iwi.dfki.de>
2018-07-06 10:02:45 +0200

    start_encoding_data

---
+++ encodeData.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+colnames = ['RowID','STATIONS_ID', 'MESS_DATUM', 'V_N_x', 'V_S1_CS', 'V_S1_HHS',
+                                        'V_S1_NS', 'V_S2_CS', 'V_S2_HHS', 'V_S2_NS', 'V_N_y', 'P', 'P0',
+                                        'R1', 'RS_IND',
+                                        'TT_TU', 'RF_TU', 'SD_SO', 'F', 'D']
+
+result = pd.read_csv('results.csv',names = colnames,  sep=',', header=1)
+
+result.drop('RowID', 1, inplace=True)
+
+# HotEncode
+
+print(result)
