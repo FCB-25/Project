d43a27f60226e472eadffe64bbd9e2c2a039f248
Alex <AlexanderDaniel.Berrang@iwi.dfki.de>
2018-07-08 12:39:13 +0200

    nn

--- Neural_Net.py
+++ Neural_Net.py
@@ -1,11 +1,36 @@
 from keras.models import Sequential
 from keras.layers import Dense
-import numpy
+import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
 
-# fix random seed for reproducibility
-numpy.random.seed(7)
+
+# copy pasted submission 3 ex1
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    """Selects a single column with index `key` from some matrix X"""
+
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self  # do nothing during fitting procedure
+
+    def transform(self, data_matrix):
+        return data_matrix[:, [self.key]]  # return a matrix with single column
+
+class OneHotEncoder(BaseEstimator, TransformerMixin):
+    """Assumes that input X to fit and transform is a single
+    column matrix of categorical values."""
+    def fit(self, X, y=None):
+        # determine unique labels
+        self.elements = np.unique(X[:, 0])
+        self.elements.sort()
+        return self
+
+    def transform(self, X, y=None):
+        return np.column_stack([X[:,0] == e for e in self.elements])*1.0
 
 """ load and prepare Data """
 
@@ -21,24 +46,44 @@ V_S2_CS = result.get('V_S2_CS').astype(str)  # [0,9]
 RS_IND = result.get('RS_IND').astype(str)  # [0,1]
 
 XY = result.values
-sel = [x for x in range(XY.shape[1]) if x != 13]
-X, y = XY[:, sel], XY[:, 13]
+
+sel = ColumnSelector(3).transform(XY)
+sel2 = ColumnSelector(6).transform(XY)
+
+coder = OneHotEncoder()
+
+coder = coder.fit(sel)
+sel = coder.transform(sel)
+
+coder = coder.fit(sel2)
+sel2 = coder.transform(sel2)
+colnames = ['sel_0','sel_1','sel_2','sel_3','sel_4','sel_5','sel_6','sel_7','sel_8']
+
+result = result.join(pd.DataFrame(sel,columns=colnames ))
+result = result.join(pd.DataFrame(sel2))
+result.drop('V_S1_CS',1, inplace=True)
+result.drop('V_S2_CS',1, inplace=True)
+
+XY = result.values
+
+sel = [x for x in range(XY.shape[1]) if x != 11]
+X, y = XY[:, sel], XY[:, 11]
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, test_size=0.25, random_state=0)
 
 # create model
 model = Sequential()
-model.add(Dense(256, input_dim=18, activation='sigmoid'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(18, activation='relu'))
+model.add(Dense(512, input_dim=34, activation='sigmoid'))
+model.add(Dense(256, activation='sigmoid'))
+model.add(Dense(34, activation='tanh'))
 model.add(Dense(1, activation='sigmoid'))
 
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-model.fit(X, y, epochs=5, batch_size=10)
+model.fit(X_train, y_train, epochs=50, batch_size=10)
 
 # evaluate the model
-scores = model.evaluate(X, y)
+scores = model.evaluate(X_test, y_test)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
